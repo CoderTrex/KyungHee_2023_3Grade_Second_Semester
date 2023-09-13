9281000649c63beef9b55ad72bd3439e5e538a79
정은성 <80687043+CoderTrex@users.noreply.github.com>
2023-09-13 16:20:13 +0900

    알고리즘 과제

--- Algorithm/Homework/hw1.py
+++ Algorithm/Homework/hw1.py
@@ -28,74 +28,101 @@ def merge_sort(arr):
     if len(arr) <= 1:
         return arr
 
-    mid = len(arr) // 2
-    low_arr = merge_sort(arr[:mid])
-    high_arr = merge_sort(arr[mid:])
+    location = len(arr) // 2
+    array_low = merge_sort(arr[:location])
+    array_high = merge_sort(arr[location:])
 
-    merged_arr = []
+    array_result = []
     low = 0
     high = 0
-    while (low < len(low_arr) and high < len(high_arr)):
-        if low_arr[low] < high_arr[high]:
-            merged_arr.append(low_arr[low])
-            low += 1
-        else:
-            merged_arr.append(high_arr[high])
+    while (low < len(array_low) and high < len(array_high)):
+        if array_low[low] > array_high[high]:
+            array_result.append(array_high[high])
             high += 1
+        else:
+            array_result.append(array_low[low])
+            low += 1
 
-    merged_arr += low_arr[low:]
-    merged_arr += high_arr[high:]
+    array_result += array_high[high:]
+    array_result += array_low[low:]
 
-    return merged_arr
+    return array_result
 
-
-
-while (1):
-    try_number = int(input("시도 횟수 직접 혹은 아래 번호를 입력하시오.\n1. 5000 2. 10000\n"))
-
-    if (try_number == 1):
+version = 0
+while version < 2:
+    print("\n\n")
+    if (version == 0):
         try_number = 5000
-    elif (try_number == 2):
+    else:
         try_number = 10000
-
     random_list = []
     for i in range(0, try_number):
         random_list.append(random.randint(1, 1000))
-    print("this is random list\n",random_list)
 
-    sort_method = int(input("실행하고자 하는 정렬 방식을 입력하시오\n1. 선택정렬 2. 병합정렬 3. 둘 다 4. 종료\n"))
-    match sort_method:
-        case 1:
-            random_list, end = selection_sort(random_list)
-            print(random_list)
-            print("\n")
-            print("time spend on selection sort: {0}".format(end))
+    random_list, end_selection = selection_sort(random_list)
+    # print("selection sort list:\n", random_list)
+    print("\n")
+
+    start = time.time()
+    merge_list = merge_sort(random_list)
+    print("merge sort starts.")
+    end_merge = time.time() - start
+    # print("merge sort list:\n", merge_list)
+    print("\n")
+
+    print("n:                            {0}".format(try_number))
+    print("time spend on selection sort: {0}".format(end_selection))
+    print("time spend on merge sort:     {0}".format(end_merge))
+    version += 1
+
+
+# 여러 번 돌리기 위해서 작성한 코드
+# while (1):
+#     try_number = int(input("시도 횟수 직접 혹은 아래 번호를 입력하시오.\n1. 5000 2. 10000\n"))
+
+#     if (try_number == 1):
+#         try_number = 5000
+#     elif (try_number == 2):
+#         try_number = 10000
+
+#     random_list = []
+#     for i in range(0, try_number):
+#         random_list.append(random.randint(1, 1000))
+#     # print("this is random list\n",random_list)
+
+#     sort_method = int(input("실행하고자 하는 정렬 방식을 입력하시오\n1. 선택정렬 2. 병합정렬 3. 둘 다 4. 종료\n"))
+#     match sort_method:
+#         case 1:
+#             random_list, end = selection_sort(random_list)
+#             print(random_list)
+#             print("\n")
+#             print("time spend on selection sort: {0}".format(end))
         
-        case 2:
-            start = time.time()
-            merge_list = merge_sort(random_list)
-            end = time.time() - start
-            print(merge_list)
-            print("\n")
-            print("time spend on merge sort: {0}".format(end))
+#         case 2:
+#             start = time.time()
+#             merge_list = merge_sort(random_list)
+#             end = time.time() - start
+#             # print(merge_list)
+#             print("\n")
+#             print("time spend on merge sort: {0}".format(end))
         
-        case 3:
-            random_list, end_selection = selection_sort(random_list)
-            print("selection sort list:\n", random_list)
-            print("\n")
+#         case 3:
+#             random_list, end_selection = selection_sort(random_list)
+#             print("selection sort list:\n", random_list)
+#             print("\n")
+#             print("merge sort starts.")
+#             start = time.time()
+#             merge_list = merge_sort(random_list)
+#             end_merge = time.time() - start
+#             print("merge sort list:\n", merge_list)
+#             print("\n")
             
-            start = time.time()
-            merge_list = merge_sort(random_list)
-            end_merge = time.time() - start
-            print("merge sort list:\n", merge_list)
-            print("\n")
+#             print("n:                            {0}".format(try_number))
+#             print("time spend on selection sort: {0}".format(end_selection))
+#             print("time spend on merge sort:     {0}".format(end_merge))
             
-            print("n:                            {0}".format(try_number))
-            print("time spend on selection sort: {0}".format(end_selection))
-            print("time spend on merge sort:     {0}".format(end_merge))
-            
-        case 4:
-            exit()
+#         case 4:
+#             exit()
         
-        case _:
-            print("wrong number.. plz input the number 1~4")
+#         case _:
+#             print("wrong number.. plz input the number 1~4")
