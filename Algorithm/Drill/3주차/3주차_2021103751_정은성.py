11cd1c7a9b16bcccaeb51b4944e011b9153798d9
정은성 <80687043+CoderTrex@users.noreply.github.com>
2023-09-20 14:13:59 +0900

    다음부터 잘하자~

--- Algorithm/Drill/3주차/3주차_2021103751_정은성.py
+++ Algorithm/Drill/3주차/3주차_2021103751_정은성.py
@@ -10,14 +10,12 @@ def bs(data, item, low, high):
         elif (data[mid] > item):
             return bs(data, item, low, mid-1)
 
-data = [1,3,5,7,9,14,17,19]
+data = [1,3,5,6,7,9,10,14,17,19]
 
 n = 10
-location = bs(data, 17, 0, n-1)
+location = bs(data, 19, 0, n-1)
 print("찾지 못하면 -1을 반환")
 print(location)
-
-
 
 def mergeSort2(array, low, high):
     width = 1
