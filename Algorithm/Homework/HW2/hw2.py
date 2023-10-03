2ba31826210321a6d6687c3a819c71e47f5c95ea
정은성 <80687043+CoderTrex@users.noreply.github.com>
2023-10-03 13:48:30 +0900

    알고리즘 과제 HW2

--- Algorithm/Homework/HW2/hw2.py
+++ Algorithm/Homework/HW2/hw2.py
@@ -1,4 +1,4 @@
-import utility
+from utility import *
 
 ######################################################################################
 ################################## 1. Floyd 알고리즘 ##################################
@@ -39,30 +39,82 @@ g=\
 
 
 print("\n1. floyd 알고리즘\n")
-utility.printMatrix(g)
+printMatrix(g)
 d, p = allShortestPath(g, 5)
 print()
-utility.printMatrix(d)
+printMatrix(d)
 print()
-utility.printMatrix(p)
+printMatrix(p)
 print()
 
 
 ######################################################################################
 ############################ 2. 연쇄행렬 최소 곱셈 알고리즘 ############################
 ######################################################################################
+# void order(index i, index j) {
+#     if (i == j) 
+#         cout << “A” << i;
+#     else {
+#         k = P[i][j];
+#         cout << “(”;
+#         order(i,k);
+#         order(k+1,j);
+#         cout << “)”;
+#     }
+# }
+
+# int minmult(int n, const int d[], index P[][]) {
+#     index i, j, k, diagonal;
+#     int M[1..n][1..n];
+#     for(i=1; i <= n; i++)
+#         M[i][i] = 0;
+#     for(diagonal = 1; diagonal <= n-1; diagonal++)
+#         for(i=1; i <= n-diagonal; i++) {
+#             j = i + diagonal;
+#             M[i][j]= minimumi<=k<=j-1(M[i][k]+M[k+1][j]+d[i-1]*d[k]*d[j]); 
+#             P[i][j] = 최소치를 주는 k의 값
+#         }
+#     return M[1][n];
+# }
+
 
 def order(p,i,j):
 # 구현
+
     return 0
 
-d=[5,2,3,4,6,7,8]
-n=len(d)-1
-m=[[0 for j in range(1,n+2)] for i in range(1,n+2)]
-p=[[0 for j in range(1,n+2)] for i in range(1,n+2)]
-# 구현
-print("\n2. 연쇄행렬 최소 곱셈 알고리즘\n")
-utility.printMatrix(m)
+
+def order(p, i, j):
+    if i == j:
+        print("A" + str(i), end='')
+    else:
+        k = p[i][j]
+        print("(", end='')
+        order(p, i, k)
+        order(p, k+1, j)
+        print(")", end='')
+
+
+d = [5,2,3,4,6,7,8]
+n = len(d) - 1
+
+m = [[0 for j in range(1, n + 2)] for i in range(1, n + 2)]
+p = [[0 for j in range(1, n + 2)] for i in range(1, n + 2)]
+
+for diagonal in range(1, n):
+    for i in range(1, n-diagonal+1):
+        j = i + diagonal
+        min_val = 1000
+        min_k = i
+        for k in range(i, j):
+            num = m[i][k] + m[k+1][j] + d[i-1]*d[k]*d[j]
+            if num < min_val:
+                min_val = num
+                min_k = k
+        m[i][j] = min_val
+        p[i][j] = min_k
+
+printMatrix(m)
 print()
-utility.printMatrix(p)
-order(p,1,6)
+printMatrix(p)
+order(p, 1, 6)
