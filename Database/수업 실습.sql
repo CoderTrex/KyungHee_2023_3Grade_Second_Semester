b1b2db7c622ad5a31dfc90f66c18bcb49b29e5f5
정은성 <80687043+CoderTrex@users.noreply.github.com>
2023-10-17 23:58:15 +0900

    SQL 실습 진행

--- Database/수업 실습.sql
+++ Database/수업 실습.sql
@@ -2,3 +2,15 @@ select d.department_name, e.first_name, j.job_title, e.salary
 from HR.employees e, HR.jobs j, HR.departments d
 where j.job_id = e.job_id and d.department_id = e.department_id
 order by d.department_id, e.salary desc;
+
+
+
+select *
+from HR.employees s, HR.jobs j
+where s.first_name = 'David' and j.job_id = s.job_id
+
+
+SELECT *
+FROM HR.employees e
+INNER JOIN HR.jobs j ON e.job_id = j.job_id
+WHERE j.job_title = (SELECT job_title FROM HR.employees WHERE first_name = 'David');
