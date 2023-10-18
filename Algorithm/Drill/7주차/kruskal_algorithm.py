cdfb1c03e72d1bcd5c0bb5f170fe4fb42cde843c
정은성 <80687043+CoderTrex@users.noreply.github.com>
2023-10-18 12:39:48 +0900

    알고리즘

--- Algorithm/Drill/7주차/kruskal_algorithm.py
+++ Algorithm/Drill/7주차/kruskal_algorithm.py
@@ -10,14 +10,16 @@ def find(v):
         parent[v] = find(parent[v])
     return parent[v]
 
+# 서로 순환이 되지 않기하기 위해서 
 def union(r1, r2):
     if r1 != r2:
         if rank[r1] > rank[r2]:
             parent[r2] = r1
             rank[r1] += rank[r2]
-    else:
-        parent[r1] = r2
-        if rank[r1] == rank[r2]: rank[r2] += rank[r1]
+        else:
+            parent[r1] = r2
+            if rank[r1] == rank[r2]: 
+                rank[r2] += rank[r1]
 
 def kruskal(graph):
     # Kruskal algorithm 구현
@@ -25,29 +27,33 @@ def kruskal(graph):
         make_singleton_set(v)
 
     mst = []
-    
     lst = sorted(graph['edges'])
-    #부모가 겹치지 않으면 서로소
+    print(lst)
     for t in lst:
+        # print(t[1], t[2])
         p = find(t[1])
         q = find(t[2])
+        # print(p, q)
+        
+        union(p,q)
+        # 서로의 부모가 같다면 순환이므로 -> 조건에 들어가지 않는다.
         if p != q:
-            union(p,q)
             mst.append(t)
             # print(mst)
     return mst
 
 graph = {
-'vertices': ['A', 'B', 'C', 'D', 'E'],
-'edges': set([
-(1, 'A', 'B'),
-(3, 'A', 'C'),
-(3, 'B', 'C'),
-(6, 'B', 'D'),
-(4, 'C', 'D'),
-(2, 'C', 'E'),
-(5, 'D', 'E'),
-])
+    'vertices': ['A', 'B', 'C', 'D', 'E'],
+    
+    'edges': set([
+    (1, 'A', 'B'),
+    (3, 'A', 'C'),
+    (3, 'B', 'C'),
+    (6, 'B', 'D'),
+    (4, 'C', 'D'),
+    (2, 'C', 'E'),
+    (5, 'D', 'E'),
+    ])
 }
 mst=kruskal(graph)
 print(mst)
--- Algorithm/Drill/7주차/prim_algorithm.py
+++ Algorithm/Drill/7주차/prim_algorithm.py
@@ -17,26 +17,19 @@ for i in range(1,n):
     nearest[i]=0
     distance[i]=w[0][i]
 
-for repeat in range(0,n-1):
+for _ in range(0,n-1):
     min = inf
-
-#검색
-    for i in range(1,n):
-        if 0<distance[i] and distance[i]<min:
+    for i in range(1, n):
+        if 0 < distance[i] and distance[i] < min:
             min = distance[i]
             vnear = i
-
-#추가
     distance[vnear] = 0 
-    F.add((vnear + 1, nearest[vnear] + 1)) 
-    # F.add((vnear + 1, nearest[vnear] + 1)) 
-
-#갱신
-
-    for i in range(1,n):
+    F.add((vnear, nearest[vnear])) 
+    # F.add((vnear + 1, nearest[vnear] + 1))
+    for i in range(1, n):
         if w[i][vnear] < distance[i]:  
             distance[i] = w[i][vnear]  
             nearest[i]=vnear
-    
+
 print()
 print(F)
