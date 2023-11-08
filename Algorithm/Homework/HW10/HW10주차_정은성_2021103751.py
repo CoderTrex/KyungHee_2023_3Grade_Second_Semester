6180c160621c275b72c1736fdad5c6ade3c6b264
정은성 <80687043+CoderTrex@users.noreply.github.com>
2023-11-08 13:48:19 +0900

    dir 최적화 및 firebase에 node.js scheduler 설치후 알아서 돌리기

--- Algorithm/Homework/HW10/HW10주차_정은성_2021103751.py
+++ Algorithm/Homework/HW10/HW10주차_정은성_2021103751.py
@@ -1,70 +1,38 @@
-# import sys
-
-# # 해의 총 개수, 
-# # 10번째 해, 
-# # 모든 해를 모두 찾기 위해 생성한 상태공간트리의 총 노드 수
-
-# void	func_backtracing(int *the_arr, int i, int *cnt)
-# {
-# 	int		the_num;
-
-# 	the_num = 0;
-# 	if (i == 9)
-# 	{
-# 		(*cnt)++;
-# 		func_printer(the_arr);
-# 	}
-# 	while (the_num < 10)
-# 	{
-# 		the_arr[i + 1] = the_num;
-# 		if (func_vaild(the_arr, i + 1))
-# 			func_backtracing(the_arr, i + 1, cnt);
-# 		the_num++;
-# 	}
-# }
-
-# int	ft_ten_queens_puzzle(void)
-# {
-# 	int		the_arr[11];
-# 	int		cnt;
-# 	int		i;
-
-# 	i = -1;
-# 	cnt = 0;
-# 	while (++i < 10)
-# 		the_arr[i] = -1;
-# 	func_backtracing(the_arr, -1, &cnt);
-# 	return (cnt);
-# }
+# 해의 총 개수, 
+# 10번째 해, 
+# 모든 해를 모두 찾기 위해 생성한 상태공간트리의 총 노드 수
 
 cnt = 0
+node = 0
 
 def promising(i,col):
-    k = 1
-    valid = True
-    
-    while (k <i and valid):
-        if (col[i] == col[k] or abs(col[i] - col[k]) == i - k):
-            valid = False
-            k += 1
-    return valid
-    # 구현
-
-
+    vaild = True
+    for k in range(i):
+        if (col[i] == col[k] or abs(col[i] - col[k]) == abs(i - k)):
+            return False
+    return vaild
 
 def queens(n, i, col): 
+    global cnt
+    global node
     if (i == n):
         cnt += 1
-        print(col)
+        if (cnt == 10):
+            print("10th          node: {0}".format(col))
+        return
+    else:
+        for k in range(n):
+            node += 1
+            col[i] = k
+            if (promising(i, col)):
+                queens(n, i+1, col)
 
-    for i in range(n):
-        col[i+1] = 0
-        if (promising(i+1, col)):
-            queens(n, i+1, col)
-
-n=5
-col=n*[0]
-queens(n,-1,col)
-
-print(cnt)
-
+n=8
+col=(n)*[0]
+start = 0
+queens(n, start, col)
+print()
+print("solution  node cnt: {0}".format(cnt))
+print()
+print("Generated node cnt: {0}".format(node))
+print()
