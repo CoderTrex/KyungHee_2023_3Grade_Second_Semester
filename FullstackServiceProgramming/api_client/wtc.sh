b22157a60a8b03eb691b3e1f41e06ac88406f444
정은성 <80687043+CoderTrex@users.noreply.github.com>
2023-11-08 14:46:47 +0900

    네이버 웹툰 api 서버 구축 완료 및 crontab 설정 완료

---
+++ FullstackServiceProgramming/api_client/wtc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# 필요한 환경 변수 설정 (예: Node.js 환경 변수)
+export PATH="$PATH:/C:/Program Files/nodejs"
+export NODE_ENV=production
+
+# 스크립트 실행
+cd /mnt/c/Code/KyungHee_2023_3Grade_Second_Semester/FullstackServiceProgramming/api_client
+node api_client.js
+
+# 실행 결과 출력
+echo "스크립트가 실행되었습니다."
