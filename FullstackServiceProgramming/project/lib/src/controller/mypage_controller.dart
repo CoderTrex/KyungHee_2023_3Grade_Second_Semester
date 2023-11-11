c997d1c4f7b08d97c16104945761592b34ebf552
정은성 <80687043+CoderTrex@users.noreply.github.com>
2023-11-11 13:44:28 +0900

    발표용 프젝 삭제

--- FullstackServiceProgramming/project/lib/src/controller/mypage_controller.dart
+++ FullstackServiceProgramming/project/lib/src/controller/mypage_controller.dart
@@ -3,34 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:project/src/controller/auth_controller.dart';
 import 'package:project/src/models/project_user_.dart';
 
-// class MyPageController extends GetxController with GetTickerProviderStateMixin {
-//   late TabController tabController;
-//   Rx<Puser> targetUser = Puser().obs;
-
-//   @override
-//   void onInit() {
-//     super.onInit();
-//     tabController = TabController(length: 2, vsync: this);
-//     _loadData();
-//   }
-
-//   void setTargetUser() {
-//     var uid = Get.parameters['targetUid'];
-//     if (uid == null) {
-//       targetUser(AuthController.to.user.value);
-//     } else {
-//       // Todo 상대 uid로 users collection을 조회
-//     }
-//   }
-
-//   void _loadData() {
-//     setTargetUser();
-
-//     // 포스트 라스트 데이터
-//     // 사용자 정보 로드
-//   }
-// }
-
 class MyPageController extends GetxController with GetTickerProviderStateMixin {
   late TabController tabController;
   Rx<Puser> targetUser = Puser().obs;
@@ -53,7 +25,6 @@ class MyPageController extends GetxController with GetTickerProviderStateMixin {
 
   void _loadData() {
     setTargetUser();
-
     // 포스트 리스트 로드
     // 사용자 정보 로드
   }
