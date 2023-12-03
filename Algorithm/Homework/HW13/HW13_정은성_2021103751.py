2e63e7432522a86347b1d5da00b7a17f7dcc6a80
정은성 <80687043+CoderTrex@users.noreply.github.com>
2023-12-04 00:16:16 +0900

    알고리즘

--- Algorithm/Homework/HW13/HW13_정은성_2021103751.py
+++ Algorithm/Homework/HW13/HW13_정은성_2021103751.py
@@ -6,6 +6,7 @@ class Heap(object):
         self.data = data
         # heap size를 하나 줄여야 한다. 인덱스는 1부터. 인덱스의 2* 연산 가능하도록.
         self.n = len(self.data) - 1
+        self.move_count = 0  # 데이터 이동 횟수를 기록할 변수
 
     def addElt(self, elt):
         # 조건 1: 데이터가 입력되는 순서대로 heap을 매번 구성
@@ -21,7 +22,8 @@ class Heap(object):
             # 부모 노드의 값과 현재 노드의 값을 교환
             self.data[i], self.data[i // 2] = self.data[i // 2], self.data[i]
             # i를 부모 노드의 인덱스로 갱신하여 계속해서 부모 방향으로 진행
-            i //= 2    
+            i //= 2
+            self.move_count += 1  # 데이터 이동 횟수 증가
 
     def siftDown(self, i):
         # siftDown 메서드는 주어진 인덱스 i에서 시작하여 자식 노드로 내려가면서
@@ -40,16 +42,15 @@ class Heap(object):
             self.data[i], self.data[j] = self.data[j], self.data[i]
             # i를 자식 노드의 인덱스로 갱신하여 계속해서 자식 방향으로 진행
             i = j
-
+            self.move_count += 1  # 데이터 이동 횟수 증가
 
     def makeHeap1(self):
         # makeHeap1 메서드는 주어진 데이터로부터 Heap을 생성 
-        # 데이터가 입력되는 순서대로 Heap을 매번 구성하는 방식
+        # 데이터가 입력되는 순서대로 Heap을 매번 구성
         for i in range(2, self.n + 1):
             # 2부터 n까지의 인덱스에 대해 siftUp 메서드를 호출하여
             # 데이터가 입력되는 순서대로 Heap을 매번 구성
             self.siftUp(i)
-
 
     def makeHeap2(self):
         # makeHeap2 메서드는 주어진 데이터로부터 Heap을 생성하는데,
@@ -77,26 +78,46 @@ class Heap(object):
         # 정렬 결과를 역순으로 반환
         return heap.data[1:][::-1]
 
+
+
+
+print("""
+
+-----------------------------------
+방법1: 데이터가 입력되는 순서대로 heap을 매번 구성
+--------------문제 1번--------------
+-----------------------------------
+
+
+""")
 # makeheap1 / 방법1: 데이터가 입력되는 순서대로 heap을 매번 구성
 a1 = [0, 11, 14, 2, 7, 6, 3, 9, 5]
 b1 = Heap(a1)
 b1.makeHeap1()
 print("Heap 1:", b1.data[1:])
+print("Move Count for Heap 1:", b1.move_count)  # 데이터 이동 횟수 출력
 
 # heapSort
 s1 = Heap.heapSort(a1)
 print("Sorted Heap 1:", s1)
 
+print("""
+
+
+-----------------------------------
+방법2: 모든 데이터를 트리에 넣은 상태에서 heap 구성
+--------------문제 2번--------------
+-----------------------------------
+
+
+""")
 # makeheap2 / 방법2: 모든 데이터를 트리에 넣은 상태에서 heap 구성
 a2 = [0, 11, 14, 2, 7, 6, 3, 9, 5]
 b2 = Heap(a2)
 b2.makeHeap2()
 print("Heap 2:", b2.data[1:])
-
+print("Move Count for Heap 2:", b2.move_count)  # 데이터 이동 횟수 출력
 # addElt
 b2.addElt(50)
 print("Heap 2 after adding 50:", b2.data[1:])
-
-# heapSort
-s2 = Heap.heapSort(a2)
-print("Sorted Heap 2:", s2)
+print("Move Count for Heap 2 after adding 50:", b2.move_count)  # 데이터 이동 횟수 출력
