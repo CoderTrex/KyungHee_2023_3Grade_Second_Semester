cc6c99019a9f17487d5db868db3cdffb4fda49bd
정은성 <80687043+Tyranno-Rex@users.noreply.github.com>
2024-07-18 17:13:03 +0900

    3-2 khu data clean

--- Database/sql/수업 실습.sql
+++ Database/sql/수업 실습.sql
@@ -3,12 +3,9 @@ from HR.employees e, HR.jobs j, HR.departments d
 where j.job_id = e.job_id and d.department_id = e.department_id
 order by d.department_id, e.salary desc;
 
-
-
 select *
 from HR.employees s, HR.jobs j
 where s.first_name = 'David' and j.job_id = s.job_id
-
 
 SELECT *
 FROM HR.employees e
